532995bb2346499ec1863cbda0a22fd9fc9e6f6c
Hovik Melikyan <hovik.melikyan@gmail.com>
2020-02-13 16:17:07 +0100

    Initial commit

---
+++ PagingScrollers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PagingScrollers
+//
+//  Created by Hovik Melikyan on 13/02/2020.
+//  Copyright © 2020 Eleven Life. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
